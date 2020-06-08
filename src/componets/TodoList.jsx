2db2dee58d010e7590eeeb847efdc94fb688a4c2
Monica Howard <mo@Monicas-Air.attlocal.net>
2020-06-08 12:30:47 -0500

    creates basic TodoList component

--- src/componets/TodoList.jsx
+++ src/componets/TodoList.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import TodoListContext from '../context'
 
 const TodoList = () => {
+
+    const {state, dispatch} = useContext(TodoListContext);
+    const subHeader = state.todos.length > 0 
+        ? `You have ${state.todos.length} unfinished tasks` :
+        "There are no items on your todo list"
+    
+
    return(
        <div>
-           <h1>from Todo List</h1>
+            <h1>Best Damn Todo List App.</h1>
+            <h3>{subHeader}</h3>
+            <ul>
+               {state.todos.map(todoItem => (
+                   <li key={todoItem.id}>
+                       {todoItem.text}
+                       <button>delete</button>
+
+                   </li>
+               ))}
+           </ul>
        </div>
    )
     
